15e29e56a29dedd150e8e2d5e17fffa2446f7a6d
Tony Miller <mcfiredrill@gmail.com>
2022-02-10 12:17:25 -0800

    add failing tests

--- tests/integration/components/fruits-ui/pagination-test.js
+++ tests/integration/components/fruits-ui/pagination-test.js
@@ -18,4 +18,30 @@ module('Integration | Component | fruits-ui/pagination', function (hooks) {
 
     assert.dom(this.element).hasText('1 2 3 4 5 ... 100 »');
   });
+
+  test('it shows the last page', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    this.page = 100;
+    this.totalPages = 100;
+    await render(
+      hbs`<FruitsUi::Pagination @page={{this.page}} @totalPages={{this.totalPages}} route="application" />`
+    );
+
+    assert.dom(this.element).hasText('« 1 2 ... 96 97 98 99 100 »');
+  });
+
+  test('it shows a page in the middle', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    this.page = 66;
+    this.totalPages = 100;
+    await render(
+      hbs`<FruitsUi::Pagination @page={{this.page}} @totalPages={{this.totalPages}} route="application" />`
+    );
+
+    assert.dom(this.element).hasText('« 1 2 ... 64 65 66 67 68... 99 100 »');
+  });
 });
